5ebea79262f5fad3bea45669b5247c50b648ccbf
sarizzm <1851712487@qq.com>
2024-07-03 08:59:09 +0800

    team.html change to js

--- js/team.js
+++ js/team.js
@@ -1,67 +1,157 @@
 var allStudentData={
     posPhD:[
         {
-            imageSrc:" ",
-            imageWithHigh:[300,500],
-            studentName:"",
-            Supervsior:"",
-            CoSupervsior:"",
-            eduBj_1:"",
-            eduBj_2:"",
-            selfEmail:""
+            imageSrc:"img/Team/lupushun.jpg",
+            imageWithHigh:[190,240],
+            studentName:"Pushun Lu",
+            Supervsior:"Supervsior：Prof. Xueliang Sun",
+            CoSupervsior:"Co-Supervisor： Prof. Changhong Wang",
+            eduBj_1:"Ph.D.: CAS (2018-2023)",
+            eduBj_2:"B.S.: Xiamen University (2014-2018)",
+            selfEmail:"plu@eitech.edu.cn"
         },
         {
-            imageSrc:" ",
-            imageWithHigh:[300,500],
-            studentName:"",
-            Supervsior:"",
-            CoSupervsior:"",
-            eduBj_1:"",
-            eduBj_2:"",
-            selfEmail:""
+            imageSrc:"img/Team/zhangjiaxu.jpg",
+            imageWithHigh:[190,240],
+            studentName:"Jiaxu Zhang",
+            Supervsior:"Supervsior：Prof. Xueliang Sun",
+            CoSupervsior:"Co-Supervisor： Prof. Changhong Wang",
+            eduBj_1:"Ph.D.: WHUT (2018-2023)",
+            eduBj_2:"B.S.: WHUT (2014-2018)",
+            selfEmail:"jxzhang@eitech.edu.cn"
         },
+        {
+            imageSrc:"img/Team/huguantai.jpg",
+            imageWithHigh:[190,240],
+            studentName:"Guantai Hu",
+            Supervsior:"Supervisor ： Prof. Changhong Wang",
+            CoSupervsior:"",
+            eduBj_1:"Ph.D.: Iowa State University (2015-2022)",
+            eduBj_2:"B.E.: USTC (2010-2014)",
+            selfEmail:"ghu@eitech.edu.cn"
+        },
+        {
+            imageSrc:"img/Team/liangsuzhe.jpg",
+            imageWithHigh:[190,240],
+            studentName:"Suzhe Liang ",
+            Supervsior:"Supervisor ： Prof. Changhong Wang",
+            CoSupervsior:"",
+            eduBj_1:"Ph.D.: TUM (2019-2024)",
+            eduBj_2:"M.S.: CNITECH-CAS (2015-2018)",
+            selfEmail:"liangsuzhe19@gmail.com"
+        },
+        // {
+        //     imageSrc:"",
+        //     imageWithHigh:[190,240],
+        //     studentName:"",
+        //     Supervsior:"",
+        //     CoSupervsior:"",
+        //     eduBj_1:"",
+        //     eduBj_2:"",
+        //     selfEmail:""
+        // },
     ],
 
     PhDs:[
         {
-            imageSrc:" ",
-            imageWithHigh:[300,500],
-            studentName:"",
-            Supervsior:"",
+            imageSrc:"img/Team/xiashengjie.jpg",
+            imageWithHigh:[170,240],
+            studentName:"Shengjie Xia",
+            Supervsior:"Supervisor： Prof. Changhong Wang",
             CoSupervsior:"",
-            eduBj_1:"",
-            eduBj_2:"",
-            selfEmail:""
+            eduBj_1:"M.S.: USTC (2019-2022)",
+            eduBj_2:"B.S.: Hefei University (2015-2019)",
+            selfEmail:"sjxia@eitech.edu.cn"
         },
         {
-            imageSrc:" ",
-            imageWithHigh:[300,500],
-            studentName:"",
-            Supervsior:"",
+            imageSrc:"img/Team/chenqian.jpg",
+            imageWithHigh:[190,240],
+            studentName:"Qian Cheng",
+            Supervsior:"Supervsior：Prof. Xueliang Sun",
+            CoSupervsior:"Co-Supervisor： Prof. Changhong Wang",
+            eduBj_1:"M.S.: SCUT (2020-2023)",
+            eduBj_2:"B.S.: Xiangtan University (2016-2020)",
+            selfEmail:"qcheng@eitech.edu.cn"
+        },
+        {
+            imageSrc:"img/Team/liuyao.jpg",
+            imageWithHigh:[190,240],
+            studentName:"Yao Liu",
+            Supervsior:"Supervisor： Prof. Changhong Wang",
             CoSupervsior:"",
-            eduBj_1:"",
-            eduBj_2:"",
-            selfEmail:""
+            eduBj_1:"M.S.: HIT (2020-2023)",
+            eduBj_2:"B.S.: NPU (2016-2020)",
+            selfEmail:"yliu@eitech.edu.cn"
+        },
+        {
+            imageSrc:"img/Team/zhouzhim.jpg",
+            imageWithHigh:[180,240],
+            studentName:"Zhimin Zhou",
+            Supervsior:"Supervsior：Prof. Xueliang Sun",
+            CoSupervsior:"Co-Supervisor： Prof. Changhong Wang",
+            eduBj_1:"M.S.: SARI-CAS (2018-2021)",
+            eduBj_2:"B.S.: NPU (2014-2018)",
+            selfEmail:"zzhou@eitech.edu.cn"
+        },
+        {
+            imageSrc:"img/Team/zhangshutao.jpg",
+            imageWithHigh:[190,240],
+            studentName:"Shutao Zhang",
+            Supervsior:"Supervisor： Prof. Changhong Wang",
+            CoSupervsior:"",
+            eduBj_1:"M.S.: CNITECH-CAS (2019-2022)",
+            eduBj_2:"B.S.: UPC (2015-2019)",
+            selfEmail:"stzhang@eitech.edu.cn"
+        },
+        {
+            imageSrc:"img/Team/wangziqing.jpg",
+            imageWithHigh:[190,240],
+            studentName:"Ziqing Wang",
+            Supervsior:"Supervisor： Prof. Changhong Wang",
+            CoSupervsior:"",
+            eduBj_1:"M.S.: Huaqiao University (2020-2023)",
+            eduBj_2:"B.S.: AAU (2016-2020)",
+            selfEmail:"zqwang@eitech.edu.cn"
+        },
+        {
+            imageSrc:"img/Team/zhumengfei.jpg",
+            imageWithHigh:[190,240],
+            studentName:"Mengfei Zhu",
+            Supervsior:"Supervisor: Prof. Xueliang Sun",
+            CoSupervsior:"Co-Supervisor: Prof. Changhong Wang",
+            eduBj_1:"M.S.: NJU (2020-2023)",
+            eduBj_2:"B.S.: BUCT (2016-2020)",
+            selfEmail:"zmf18810605302@163.com"
         }
     ],
 
     RAs:[
         {
-            imageSrc:"img/Team/hongjian.jpg",
+            imageSrc:"img/Team/zhangshutao.jpg",
             imageWithHigh:[190,240],
-            studentName:"Jian Hong",
-            Supervsior:"Supervisor：Prof. Changhong Wang",
-            CoSupervsior:"Supervisor：Prof. Changhong Wang",
-            eduBj_1:"M.S.: CNITECH-CAS (2020-2023)",
-            eduBj_2:"B.S.: SDU (2016-2020)",
-            selfEmail:"jhong@eitech.edu.cn"
+            studentName:"Shutao Zhang",
+            Supervsior:"Supervisor： Prof. Changhong Wang",
+            CoSupervsior:"",
+            eduBj_1:"M.S.: CNITECH-CAS (2019-2022)",
+            eduBj_2:"B.S.: UPC (2015-2019)",
+            selfEmail:"stzhang@eitech.edu.cn"
+        },
+        {
+            imageSrc:"img/Team/wangziqing.jpg",
+            imageWithHigh:[190,240],
+            studentName:"Ziqing Wang",
+            Supervsior:"Supervisor： Prof. Changhong Wang",
+            CoSupervsior:"",
+            eduBj_1:"M.S.: Huaqiao University (2020-2023)",
+            eduBj_2:"B.S.: AAU (2016-2020)",
+            selfEmail:"zqwang@eitech.edu.cn"
         },
         {
             imageSrc:"img/Team/hongjian.jpg",
             imageWithHigh:[190,240],
             studentName:"Jian Hong",
             Supervsior:"Supervisor：Prof. Changhong Wang",
-            CoSupervsior:"Supervisor：Prof. Changhong Wang",
+            CoSupervsior:"",
             eduBj_1:"M.S.: CNITECH-CAS (2020-2023)",
             eduBj_2:"B.S.: SDU (2016-2020)",
             selfEmail:"jhong@eitech.edu.cn"
@@ -77,11 +167,83 @@ window.onload = function (){
 
     function LoadData(){
         // 获取页面导航的元素对象
-        var allDiv = document.querySelector('.phd_bj');
-        var RAsStu = allStudentData.RAs;
-        console.log(allDiv);
-        console.log(RAsStu);
+        
+        var posPhDDiv = document.querySelector('.posdoc_bj');
+        var posPhDStu = allStudentData.posPhD;
 
+        
+        // console.log(allDiv);
+        // console.log(RAsStu);
+        // posPhD
+        for (let j = 0; j < posPhDStu.length; j++) {
+
+            let divNode_line = document.createElement('div');
+    
+            divNode_line.className = "posdoc_bj_des";
+            let imgNode = document.createElement('img');
+            let aNode = document.createElement('a');
+            imgNode.src=posPhDStu[j].imageSrc;
+            imgNode.width=posPhDStu[j].imageWithHigh[0];
+            imgNode.height=posPhDStu[j].imageWithHigh[1];
+            let pNode = document.createElement('p');
+            
+            pNode.append(posPhDStu[j].studentName);
+            let brNode = document.createElement('br');
+            pNode.appendChild(brNode);
+            pNode.append(posPhDStu[j].Supervsior,document.createElement('br'));
+            
+            if (posPhDStu[j].CoSupervsior&& posPhDStu[j].CoSupervsior.trim().length > 0){
+                pNode.append(posPhDStu[j].CoSupervsior,document.createElement('br'));
+            }
+            
+            pNode.append(posPhDStu[j].eduBj_1,document.createElement('br'));
+            pNode.append(posPhDStu[j].eduBj_2,document.createElement('br'));
+            aNode.href="mailto:"+posPhDStu[j].selfEmail;
+            aNode.append(posPhDStu[j].selfEmail);
+
+            pNode.append(aNode,document.createElement('br'));
+            divNode_line.append(imgNode,pNode)
+            // console.log(pNode)
+            posPhDDiv.append(divNode_line)
+        }
+
+
+        var PhDDiv = document.querySelector('.phd_bj');
+        var PhDStu = allStudentData.PhDs;
+        for (let j = 0; j < PhDStu.length; j++) {
+
+            let divNode_line = document.createElement('div');
+    
+            divNode_line.className = "phd_researcher";
+            let imgNode = document.createElement('img');
+            let aNode = document.createElement('a');
+            imgNode.src=PhDStu[j].imageSrc;
+            imgNode.width=PhDStu[j].imageWithHigh[0];
+            imgNode.height=PhDStu[j].imageWithHigh[1];
+            let pNode = document.createElement('p');
+            
+            pNode.append(PhDStu[j].studentName);
+            let brNode = document.createElement('br');
+            pNode.appendChild(brNode);
+            pNode.append(PhDStu[j].Supervsior,document.createElement('br'));
+            
+            if (PhDStu[j].CoSupervsior&& PhDStu[j].CoSupervsior.trim().length > 0){
+                pNode.append(PhDStu[j].CoSupervsior,document.createElement('br'));
+            }
+            
+            pNode.append(PhDStu[j].eduBj_1,document.createElement('br'));
+            pNode.append(PhDStu[j].eduBj_2,document.createElement('br'));
+            aNode.href="mailto:"+PhDStu[j].selfEmail;
+            aNode.append(PhDStu[j].selfEmail);
+
+            pNode.append(aNode,document.createElement('br'));
+            divNode_line.append(imgNode,pNode)
+            // console.log(pNode)
+            PhDDiv.append(divNode_line)
+        }
+        var RADiv = document.querySelector('.assistent_bj');
+        var RAsStu = allStudentData.RAs;
+        // RA 
         for (let j = 0; j < RAsStu.length; j++) {
 
             let divNode_line = document.createElement('div');
@@ -99,8 +261,10 @@ window.onload = function (){
             pNode.appendChild(brNode);
             pNode.append(RAsStu[j].Supervsior,document.createElement('br'));
             
+            if (RAsStu[j].CoSupervsior&& RAsStu[j].CoSupervsior.trim().length > 0){
+                pNode.append(RAsStu[j].CoSupervsior,document.createElement('br'));
+            }
             
-            pNode.append(RAsStu[j].CoSupervsior,document.createElement('br'));
             pNode.append(RAsStu[j].eduBj_1,document.createElement('br'));
             pNode.append(RAsStu[j].eduBj_2,document.createElement('br'));
             aNode.href="mailto:"+RAsStu[j].selfEmail;
@@ -108,8 +272,8 @@ window.onload = function (){
 
             pNode.append(aNode,document.createElement('br'));
             divNode_line.append(imgNode,pNode)
-            console.log(pNode)
-            allDiv.append(divNode_line)
+            // console.log(pNode)
+            RADiv.append(divNode_line)
         }
         
 
